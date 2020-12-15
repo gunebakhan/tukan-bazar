107f9480804f2dce449f1ba4598a3fd47a4f84b8
saeed <saeed@pop-os.localdomain>
2020-12-15 18:40:01 +0330

    user.admin.py file is upgraded

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.forms import AdminPasswordChangeForm
+from .models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import ugettext_lazy as _
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ('email',)
+    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'last_login')
+    fieldsets = (
+        (_('authentication data'), {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'username', 'avatar')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
+
+
+    
