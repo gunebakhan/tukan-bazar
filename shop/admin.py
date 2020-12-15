64475f4b6f7e8c776da4c11ef612cc3815827e91
saeed <saeed@pop-os.localdomain>
2020-12-15 19:13:55 +0330

    shop model admin is updated

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+@admin.register(Shop)
+class ShopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'page')
+
+
